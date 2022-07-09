ee2290e0ebdb9ce22273677bbc48fbb71f6b4d14
vBedar <98189283+vBedar@users.noreply.github.com>
2022-07-09 14:27:07 -0500

    Add files via upload
    This is my Django framework. We'll likely combine our work into one project at some point but for now we'll upload separate projects

---
+++ Assignment 3/clientForm/cForm/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+
+from .models import Client
+from .forms import clientForm
+
+def index(request):
+    return HttpResponseRedirect('registration/')
+    #clientregistration = Client
+   # context = { 
+    #    'clientregistration' : clientregistration
+    #}
+    #return render(request, 'cForm/ClientForm.html')
+
+def formComplete(request):
+    return HttpResponse("Form Submitted, Thanks!")
+
+def submitForm(request):
+    clientregistration = Client
+    if request.method == 'POST':
+        form = clientForm(request.POST)
+        if form.is_valid():
+            clientregistration.name = form.cleaned_data['full_name']
+            clientregistration.stAddress1 = form.cleaned_data['address_1']
+            clientregistration.stAddress2 = form.cleaned_data['address_2']
+            clientregistration.city = form.cleaned_data['city']
+            clientregistration.state = form.cleaned_data['state']
+            clientregistration.zip = form.cleaned_data['zipcode']
+            #clientregistration.save()
+            return HttpResponseRedirect('complete/')
+
+    else:
+        form = clientForm()
+
+    return render(request, 'cForm/ClientForm.html', {'form': form})    
+
