7554cf2f4a98606e325648e0579fdc4c3ecfabf3
vBedar <vbedar@CougarNet.UH.EDU>
2022-07-20 13:58:44 -0500

    Added Foreign-Key to client # for now

--- Assignment4/FuelProject/FuelSite/models.py
+++ Assignment4/FuelProject/FuelSite/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# The Client Model for the ClientInformation Table
+# UserCredentials and ClientInformation are one-to-one
 class Client (models.Model):
+    user = models.ForeignKey('User', on_delete = models.RESTRICT)
     stAddress1 = models.CharField(max_length = 100)
     stAddress2 = models.CharField(max_length = 100, blank=True)
     city = models.CharField(max_length = 100)
