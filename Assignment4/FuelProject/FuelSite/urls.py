68d18de1a2d895aca6370aa05f65ea7885bf167d
vBedar <vbedar@CougarNet.UH.EDU>
2022-07-21 16:29:40 -0500

    Testing, Need user model to test client model

--- Assignment4/FuelProject/FuelSite/urls.py
+++ Assignment4/FuelProject/FuelSite/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('registration/', views.submitForm, name = 'submitForm'),
-    # path('registration/complete/', views.formComplete, name = 'formComplete')
+    #path('<int:user_id>/registration/', views.submitForm, name = 'submitForm'),
+    #path('<int:user_id>/registration/complete/', views.formComplete, name = 'formComplete')
 ]
--- Assignment4/FuelProject/FuelSite/views.py
+++ Assignment4/FuelProject/FuelSite/views.py
@@ -3,30 +3,40 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
 from .models import Client
 from .forms import clientForm
 
 def index(request):
     #return HttpResponse()
+    #return HttpResponseRedirect('registration/')
+    #return HttpResponseRedirect(reverse('submitForm'), args)
 
-# def formComplete(request):
-#     return HttpResponse("Form Submitted, Thanks!")
-def submitForm(request):
-    clientregistration = Client
+def formComplete(request):
+    return HttpResponse("Form Submitted, Thanks!")
+
+def submitForm(request, user_id):
+    client_detail = get_object_or_404(Client, pk=user_id)
     if request.method == 'POST':
         form = clientForm(request.POST)
         if form.is_valid():
-            clientregistration.name = form.cleaned_data['full_name']
-            clientregistration.stAddress1 = form.cleaned_data['address_1']
-            clientregistration.stAddress2 = form.cleaned_data['address_2']
-            clientregistration.city = form.cleaned_data['city']
-            clientregistration.state = form.cleaned_data['state']
-            clientregistration.zip = form.cleaned_data['zipcode']
-            clientregistration.save()
-            return HttpResponseRedirect('complete/')
+            client_detail.name = form.cleaned_data['full_name']
+            client_detail.stAddress1 = form.cleaned_data['address_1']
+            client_detail.stAddress2 = form.cleaned_data['address_2']
+            client_detail.city = form.cleaned_data['city']
+            client_detail.state = form.cleaned_data['state']
+            client_detail.zip = form.cleaned_data['zipcode']
+            client_detail.save()
+            return HttpResponseRedirect('complete/') 
+            #return HttpResponseRedirect(reverse('formComplete', args = (user.id)))
 
     else:
         form = clientForm()
 
-    return render(request, 'ClientForm.html', {'form': form})    
+    context = {
+        'form': form,
+        'client_detail': client_detail,
+    }
+
+    return render(request, 'ClientForm.html', context)    
